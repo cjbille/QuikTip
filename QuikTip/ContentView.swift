9cfdc199ae99e16a48de6d8cb31b2e19ad26b900
Chris Bille <cbille1@icloud.com>
2026-04-11 09:12:20 -0400

    Initial Commit

---
+++ QuikTip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuikTip
+//
+//  Created by Christopher Bille on 4/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuikTip/QuikTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuikTipApp.swift
+//  QuikTip
+//
+//  Created by Christopher Bille on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct QuikTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
